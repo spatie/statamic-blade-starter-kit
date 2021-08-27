e3beebc8b2065329b1f93afc6d3dfafd3897985f
Rias <hey@rias.be>
2021-08-27 09:53:23 +0200

    Initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ $site->shortLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{ $title ?? $site->name() }}</title>
+        <link rel="stylesheet" href="{{ mix('css/tailwind.css') }}">
+    </head>
+    <body class="bg-gray-100 font-sans leading-normal text-gray-800">
+        <div class="mx-auto px-2 h-screen flex items-center justify-center">
+            @yield('content')
+        </div>
+        <script src="{{ mix('/js/site.js') }}"></script>
+    </body>
+</html>
